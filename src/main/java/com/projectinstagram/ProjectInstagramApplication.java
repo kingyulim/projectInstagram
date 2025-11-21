a1e3cba6a5b5d20df55b350d6180c21d9bb3db35
Kingyul Kim <youremail@example.com>
2025-11-21 20:12:27 +0900

    Initial commit

---
+++ src/main/java/com/projectinstagram/ProjectInstagramApplication.java
@@ -0,0 +1,13 @@
+package com.projectinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectInstagramApplication.class, args);
+    }
+
+}
