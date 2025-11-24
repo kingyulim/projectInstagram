94f6fb4faf3fc0ff43d78af52d19468f0b1b0419
Kingyul Kim <youremail@example.com>
2025-11-24 19:50:36 +0900

    ✨ feat: 커스텀 예외처리 예외 추가 NO_MEMBER_INFO - NOT_FOUND INVALID_MEMBER_INFO - BAD_REQUEST

--- src/main/java/com/projectinstagram/common/exception/ExceptionMessageEnum.java
+++ src/main/java/com/projectinstagram/common/exception/ExceptionMessageEnum.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum ExceptionMessageEnum {
     NO_MEMBER_ID(HttpStatus.NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다."),
+    NO_MEMBER_INFO(HttpStatus.NOT_FOUND, "해당 회원 정보를 찾을 수 없습니다."),
+    INVALID_MEMBER_INFO(HttpStatus.BAD_REQUEST, "회원 정보가 일치하지 않습니다."),
     NO_LOGIN(HttpStatus.UNAUTHORIZED, "로그인이 되지 않았습니다."),
     BOARD_NOT_FOUND_EXCEPTION(HttpStatus.BAD_REQUEST, "해당 게시물은 존재하지 않습니다."),
     POST_ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "접근 불가능한 게시물입니다."),
