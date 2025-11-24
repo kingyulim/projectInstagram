088898924a684e2d745d79a84dd2e9dc7b443fda
Kingyul Kim <youremail@example.com>
2025-11-24 19:48:58 +0900

    ✨ feat: UserRepository 기능 추가
    findByEmail 기능 추가 = users 테이블에 email 데이터와 같은 값이 존재 한다면 거기에 해당하는 모든 필드 데이터 값 반환

--- src/main/java/com/projectinstagram/domain/user/repository/UserRepository.java
+++ src/main/java/com/projectinstagram/domain/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.projectinstagram.domain.user.repository;
 import com.projectinstagram.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
     /**
      * 같은 이메일이 존재하는지 검사
@@ -17,5 +19,12 @@ public interface UserRepository extends JpaRepository<User,Long> {
      * @return SELECT 1 FROM users WHERE nickname = ?
      */
     boolean existsByNickname(String nickname);
+
+    /**
+     * email 데이터가 존재한다면 그에대한 모든 필드 값 반환
+     * @param email request eamil 파라미터
+     * @return SELECT * FROM users WHERE email = ?
+     */
+    Optional<User> findByEmail(String email);
 
 }
