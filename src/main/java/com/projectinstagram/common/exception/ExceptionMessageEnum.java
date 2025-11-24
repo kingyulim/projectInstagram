1b8dfa3504a7004550910de5982340f2f4daf030
Kingyul Kim <youremail@example.com>
2025-11-24 09:50:53 +0900

    ✨ feat: 커스텀 예외처리 기본 코드 추가

--- src/main/java/com/projectinstagram/common/exception/ExceptionMessageEnum.java
+++ src/main/java/com/projectinstagram/common/exception/ExceptionMessageEnum.java
@@ -7,7 +7,11 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ExceptionMessageEnum {
-    NO_MEMBER_ID(HttpStatus.NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다.");
+    NO_MEMBER_ID(HttpStatus.NOT_FOUND, "해당 ID의 회원을 찾을 수 없습니다."),
+    NO_LOGIN(HttpStatus.UNAUTHORIZED, "로그인이 되지 않았습니다."),
+    BOARD_NOT_FOUND_EXCEPTION(HttpStatus.BAD_REQUEST, "해당 게시물은 존재하지 않습니다."),
+    POST_ACCESS_DENIED_EXCEPTION(HttpStatus.UNAUTHORIZED, "접근 불가능한 게시물입니다.")
+    ;
 
     private final HttpStatus status;
     private final String message;
