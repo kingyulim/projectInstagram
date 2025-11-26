36fdd5d529f4b3ff77c82a7ffec4cf8ed8427fcb
Kingyul Kim <youremail@example.com>
2025-11-26 12:30:59 +0900

    ✨ feat: 로그인 세션 추가

--- src/main/java/com/projectinstagram/domain/user/controller/UserController.java
+++ src/main/java/com/projectinstagram/domain/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.projectinstagram.domain.user.controller;
 
+import com.projectinstagram.common.exception.CustomException;
+import com.projectinstagram.common.exception.ExceptionMessageEnum;
 import com.projectinstagram.domain.user.dto.request.JoinUserRequest;
 import com.projectinstagram.domain.user.dto.request.AcountUserRequest;
 import com.projectinstagram.domain.user.dto.response.JoinUserResponse;
 import com.projectinstagram.domain.user.dto.response.LoginUserResponse;
+import com.projectinstagram.domain.user.dto.response.SessionResponse;
 import com.projectinstagram.domain.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -34,8 +38,32 @@ public class UserController {
      * @return UserLoginResponseDto json 반환
      */
     @PostMapping("/login")
-    public ResponseEntity<LoginUserResponse> login(@Valid @RequestBody AcountUserRequest request) {
+    public ResponseEntity<LoginUserResponse> login(
+            @Valid @RequestBody AcountUserRequest request,
+            HttpSession session
+    ) {
+        SessionResponse thisSession = (SessionResponse) session.getAttribute("thisSession");
 
-        return ResponseEntity.status(HttpStatus.OK).body(userService.login(request));
+        /**
+         * thisSession 세션 없으면 예외처리
+         */
+        if (thisSession != null) {
+            throw new CustomException(ExceptionMessageEnum.LOGIN_CHECK);
+        }
+
+        LoginUserResponse loginUserResponse = userService.login(request);
+
+        SessionResponse sessionResponse = new SessionResponse(
+                loginUserResponse.getId(),
+                loginUserResponse.getName(),
+                loginUserResponse.getNickname()
+        );
+
+        /**
+         * 세션 생성
+         */
+        session.setAttribute("thisSession", sessionResponse);
+
+        return ResponseEntity.status(HttpStatus.OK).body(loginUserResponse);
     }
 }
