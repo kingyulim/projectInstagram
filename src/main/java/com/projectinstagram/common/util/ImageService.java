c9bd1ed5b529460a163e2db374e9446de4d02f48
KWON-WOO <ckw075@naver.com>
2025-11-26 18:03:59 +0900

    ✨ feat: 파일 삭제 기능 추가
    단건 삭제, 리스트 삭제 기능 추가.
    반환 값은 Boolean

--- src/main/java/com/projectinstagram/common/util/ImageService.java
+++ src/main/java/com/projectinstagram/common/util/ImageService.java
@@ -1,5 +1,7 @@
 package com.projectinstagram.common.util;
 
+import com.projectinstagram.common.exception.CustomException;
+import com.projectinstagram.common.exception.ExceptionMessageEnum;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,16 +15,16 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
-public class ImageStoreService {
+public class ImageService {
 
     //단건 저장.
-    public String store(ImageUrl url, MultipartFile file){
+    public String store(ImageUrl url, MultipartFile file) {
         String fileName = generateFileName(file);
         Path filePath = buildFilePath(url, fileName);
 
 
         // 디렉토리가 없을 시 디렉토리를 추가해주는 기능과 저장 기능.
-        try{
+        try {
             Files.createDirectories(filePath.getParent());
             file.transferTo(filePath.toFile());
         } catch (IOException e) {
@@ -31,21 +33,50 @@ public class ImageStoreService {
 
         return buildPublicUrl(url, fileName);
     }
+
     //리스트용
     public List<String> storeAll(ImageUrl url, List<MultipartFile> files) {
         List<String> result = new ArrayList<>();
-        for (MultipartFile file: files) {
-            result.add(store(url,file));
+        for (MultipartFile file : files) {
+            result.add(store(url, file));
         }
         return result;
     }
+
     //배열용
     public List<String> storeAll(ImageUrl url, MultipartFile[] files) {
         return storeAll(url, Arrays.asList(files));
     }
 
+    //삭제
+    public boolean delete(ImageUrl url, String fileName){
+        Path filePath = buildFilePath(url, fileName);
+        try {
+            Files.deleteIfExists(filePath);
+        } catch (IOException e) {
+            throw new CustomException(ExceptionMessageEnum.FAILED_DELETE_FILE);
+        }
+        return true;
+    }
+
+    //리스트용
+    public List<Boolean> deleteAll(ImageUrl url, List<String> files) {
+        List<Boolean> result = new ArrayList<>();
+        for (String file : files) {
+            result.add(delete(url, file));
+        }
+        return result;
+    }
+
+    //배열용
+    public List<Boolean> deleteAll(ImageUrl url, String[] files) {
+        return deleteAll(url, Arrays.asList(files));
+    }
+
+
+
     //파일명 겹치지 않도록 고유값을 추가하여 파일명 지정.
-    private String generateFileName(MultipartFile file){
+    private String generateFileName(MultipartFile file) {
         return UUID.randomUUID() + "_" + file.getOriginalFilename();
     }
 
@@ -53,6 +84,7 @@ public class ImageStoreService {
     private Path buildFilePath(ImageUrl url, String fileName) {
         return Paths.get(url.getUrl(), fileName);
     }
+
     // 반환해줄 주소와 파일명.
     private String buildPublicUrl(ImageUrl url, String fileName) {
         String path = url.getUrl();
