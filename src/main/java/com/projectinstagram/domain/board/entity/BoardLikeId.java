5f138ab1fe84a94c84619be08e98a50dfbce6ba7
KWON-WOO <ckw075@naver.com>
2025-11-26 14:13:37 +0900

    ♻ refactor: 타 브랜치에서 담당할 중복 클래스 삭제

--- src/main/java/com/projectinstagram/domain/board/entity/BoardLikeId.java
+++
@@ -1,9 +0,0 @@
-package com.projectinstagram.domain.board.entity;
-
-import jakarta.persistence.Embeddable;
-
-@Embeddable
-public class BoardLikeId {
-    private Long boardId;
-    private Long userId;
-}
