b857ec2c5657cc01ae05a5a1d82fd5d25c6363d9
WonTaeJun <dnjsxonws94@gmail.com>
2025-11-27 19:51:47 +0900

    ✨ feat: 데이터이니셜라이저 생성~!~!~!~!
    🐛 fix: 익셉션 입력오류 수정

--- src/main/java/com/projectinstagram/common/util/DataInitializer.java
+++ src/main/java/com/projectinstagram/common/util/DataInitializer.java
@@ -1,19 +1,33 @@
 package com.projectinstagram.common.util;
 
+import com.projectinstagram.domain.board.dto.CreateBoardRequest;
+import com.projectinstagram.domain.board.entity.Board;
+import com.projectinstagram.domain.board.repository.BoardRepository;
+import com.projectinstagram.domain.comment.entity.Comment;
+import com.projectinstagram.domain.comment.repository.CommentRepository;
 import com.projectinstagram.domain.friend.entity.Friend;
 import com.projectinstagram.domain.friend.repository.FriendRepository;
+import com.projectinstagram.domain.like.entity.BoardLike;
+import com.projectinstagram.domain.like.entity.CommentLike;
+import com.projectinstagram.domain.like.repository.BoardLikeRepository;
+import com.projectinstagram.domain.like.repository.CommentLikeRepository;
 import com.projectinstagram.domain.user.entity.User;
 import com.projectinstagram.domain.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-/*데이터더미 세팅 후 테스트, 추후 제거*/
+
+
 @Component
 @AllArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
     private final FriendRepository friendRepository;
+    private final BoardRepository boardRepository;
+    private final CommentRepository commentRepository;
+    private final BoardLikeRepository boardLikeRepository;
+    private final CommentLikeRepository commentLikeRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -24,6 +38,12 @@ public class DataInitializer implements CommandLineRunner {
         User user3 = new User("test@email.com3", "test_id3", "김테스트3","1234");
         User user4 = new User("test@email.com4", "test_id4", "김테스트4","1234");
         User user5 = new User("test@email.com5", "test_id5", "김테스트5","1234");
+        // 유저 생성
+        userRepository.save(user1);
+        userRepository.save(user2);
+        userRepository.save(user3);
+        userRepository.save(user4);
+        userRepository.save(user5);
 
 
         //친구관계 준비
@@ -34,23 +54,6 @@ public class DataInitializer implements CommandLineRunner {
         Friend Friend32 = new Friend(user3, user2);
         Friend Friend42 = new Friend(user4, user2);
         Friend Friend52 = new Friend(user5, user2);
-        //팔로잉 준비
-
-//       public User (String email, String nickname, String name, String password) {
-//            this.email = email;
-//            this.nickname = nickname;
-//            this.name = name;
-//            this.password = password;
-//        }
-
-
-        // 유저 생성
-        userRepository.save(user1);
-        userRepository.save(user2);
-        userRepository.save(user3);
-        userRepository.save(user4);
-        userRepository.save(user5);
-
         //친구관계 생성
         friendRepository.save(Friend12);
         friendRepository.save(Friend13);
@@ -59,6 +62,56 @@ public class DataInitializer implements CommandLineRunner {
         friendRepository.save(Friend32);
         friendRepository.save(Friend42);
         friendRepository.save(Friend52);
+
+
+        //게시물 준비
+        CreateBoardRequest boardDto1 = new CreateBoardRequest() {
+            @Override
+            public String getContent() {
+                return "게시물1 내용1";
+            }
+        };
+        CreateBoardRequest boardDto2 = new CreateBoardRequest() {
+            @Override
+            public String getContent() {
+                return "게시물2 내용2";
+            }
+        };
+        Board board1 = new Board(user1, boardDto1);
+        Board board2 = new Board(user1, boardDto2);
+        //게시물 생성
+        boardRepository.save(board1);
+        boardRepository.save(board2);
+
+        //게시물 좋아요
+        BoardLike boardLike2 = new BoardLike(board1, user2);
+        BoardLike boardLike3 = new BoardLike(board1, user3);
+        //게시물 좋아요생성
+        boardLikeRepository.save(boardLike2);
+        boardLikeRepository.save(boardLike3);
+
+        //댓글 준비
+        Comment comment1 = new Comment(board1, user1, "댓글");
+        Comment comment2 = new Comment(board1, user1, "댓글");
+        Comment comment3 = new Comment(board1, user1, "댓글");
+        Comment comment4 = new Comment(board1, user1, "댓글");
+        //댓글 생성
+        commentRepository.save(comment1);
+        commentRepository.save(comment2);
+        commentRepository.save(comment3);
+        commentRepository.save(comment4);
+
+        //댓글 좋아요
+        CommentLike commentLike2 = new CommentLike(comment1, user2);
+        CommentLike commentLike3 = new CommentLike(comment1, user3);
+        CommentLike commentLike4 = new CommentLike(comment1, user4);
+        //댓글 좋아요 생성
+        commentLikeRepository.save(commentLike2);
+        commentLikeRepository.save(commentLike3);
+        commentLikeRepository.save(commentLike4);
+
+
+
 
     }
 }
--- src/main/java/com/projectinstagram/domain/friend/service/FriendService.java
+++ src/main/java/com/projectinstagram/domain/friend/service/FriendService.java
@@ -1,6 +1,7 @@
 package com.projectinstagram.domain.friend.service;
 
 import com.projectinstagram.common.exception.CustomException;
+import com.projectinstagram.common.exception.ExceptionMessageEnum;
 import com.projectinstagram.domain.friend.dto.*;
 import com.projectinstagram.domain.friend.entity.Friend;
 import com.projectinstagram.domain.friend.entity.FriendId;
@@ -16,8 +17,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.projectinstagram.common.exception.ExceptionMessageEnum.*;
 
 @Slf4j
 @Service
@@ -40,11 +39,11 @@ public class FriendService {
     //region 팔로우 언팔로우 공통정보 준비매서드 (예외처리포함)
     private FriendInfo prepareFriendInfo(CreateRequest request, Long userIdFrom) {
         Long userIdTo = request.getUserIdTo();
-        if (userIdFrom == null || userIdTo == null) {throw new CustomException(NO_MEMBER_ID);}
-        if (userIdTo.equals(userIdFrom)) {throw new CustomException(SELF_FOLLOW_EXCEPTION);} //스스로를 친구할 수 없는 기능
+        if (userIdFrom == null || userIdTo == null) {throw new CustomException(ExceptionMessageEnum.NO_MEMBER_ID);}
+        if (userIdTo.equals(userIdFrom)) {throw new CustomException(ExceptionMessageEnum.SELF_FOLLOW_EXCEPTION);} //스스로를 친구할 수 없는 기능
 
-        User userTo = userRepository.findById(userIdTo).orElseThrow(() -> new CustomException(NO_MEMBER_ID));
-        User userFrom = userRepository.findById(userIdFrom).orElseThrow(() -> new CustomException(NO_MEMBER_ID));
+        User userTo = userRepository.findById(userIdTo).orElseThrow(() -> new CustomException(ExceptionMessageEnum.NO_MEMBER_ID));
+        User userFrom = userRepository.findById(userIdFrom).orElseThrow(() -> new CustomException(ExceptionMessageEnum.NO_MEMBER_ID));
 
         FriendId friendId = new FriendId(userIdFrom, userIdTo);
         boolean isFriended = friendRepository.existsById(friendId);//이미 친구인지 확인 후 boolean을 return값에 전달
@@ -56,7 +55,7 @@ public class FriendService {
     //region 친구추가 (팔로우)
     public CreateResponse follow (CreateRequest request, Long userIdFrom/*토큰으로부터 받은 값(수정필요)*/) {
         FriendInfo friendinfo = prepareFriendInfo(request, userIdFrom);
-        if (friendinfo.isFriended) {throw new CustomException(ALREADY_FRIEND_EXCEPTION);} //이미 친구인지 확인
+        if (friendinfo.isFriended) {throw new CustomException(ExceptionMessageEnum.ALREADY_FRIEND_EXCEPTION);} //이미 친구인지 확인
         Friend friend = new Friend(friendinfo.getUserFrom(), friendinfo.getUserTo());
         friendRepository.save(friend);
         return new CreateResponse(userIdFrom); /*토큰값으로부터 Long으로 변환필요*/
@@ -66,7 +65,7 @@ public class FriendService {
     //region 친구삭제 (언팔로우)
     public void unfollow(CreateRequest request, Long userIdFrom/*토큰으로부터 받은 값(수정필요)*/) {
         FriendInfo friendinfo = prepareFriendInfo(request, userIdFrom);
-        if (!friendinfo.isFriended) {throw new CustomException(NOT_FRIEND_EXCEPTION);} //친구가 아닌지 확인
+        if (!friendinfo.isFriended) {throw new CustomException(ExceptionMessageEnum.NOT_FRIEND_EXCEPTION);} //친구가 아닌지 확인
         friendRepository.deleteById(friendinfo.friendId);
     }
     //endregion
@@ -85,7 +84,7 @@ public class FriendService {
 
         for (Friend friend : followerList) {
             User user = userRepository.findById(friend.getId().getUserIdFrom()) // 팔로워 : getUserIdFrom()
-                    .orElseThrow(() -> new CustomException(NO_MEMBER_ID));
+                    .orElseThrow(() -> new CustomException(ExceptionMessageEnum.NO_MEMBER_ID));
             ReadUserResponse dto = new ReadUserResponse(
                     user.getId(),
                     user.getNickname(),
@@ -113,7 +112,7 @@ public class FriendService {
 
         for (Friend friend : followingList) {
             User user = userRepository.findById(friend.getId().getUserIdTo()) // 팔로잉 : getUserIdTo()
-                    .orElseThrow(() -> new CustomException(NO_MEMBER_ID));
+                    .orElseThrow(() -> new CustomException(ExceptionMessageEnum.NO_MEMBER_ID));
             ReadUserResponse dto = new ReadUserResponse(
                     user.getId(),
                     user.getNickname(),
